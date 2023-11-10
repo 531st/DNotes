511fcfae9580c97ab37fed297792cfbaf4950ac5
Sasface <sasface88@gmail.com>
2023-11-10 13:51:58 +0300

    status panel styles

--- src/components/Status.svelte
+++ src/components/Status.svelte
@@ -2,6 +2,11 @@
     export let currentHp = 22;
     export let maxHp=27;
     export let tempHp = 4;
+    export let spellSlots = [
+        {lvl: 1, currentVal: 1, maxVal: 4},
+        {lvl: 2, currentVal: 2, maxVal: 3},
+        {lvl: 3, currentVal: 2, maxVal: 2},
+    ];
 </script>
 
 <div class="status-container">
@@ -19,8 +24,20 @@
             </div>
         </div>
         <div class="spell-panel">
-            <p>Spell Slots</p>
+            <p>ПЕРЕДЕЛОЙ</p>
+                {#each spellSlots as slot}
+                    <div class="spell-row">
+                        
+                    </div>
+                {/each}
+                <div>
+                    <button class="spell-btn">-</button>
+                    <button class="spell-btn">+</button>
+                </div>
         </div>
+    </div>
+    <div class="notes-panel">
+        here
     </div>
 </div>
 
@@ -28,24 +45,28 @@
     .status-container{
         font-variant : small-caps;
         min-width: 36vh;
-    }
+        display: flex;
+        flex-direction: row;
+        gap: 15px;
+        justify-content: space-between;
+    }   
 
     .hp-spell-col {
         display: flex;
         flex-direction: column;
-        gap: 7px;
+        gap: 15px;
     }
 
     .hp-panel{
         display: flex;
         flex-direction: column;
-        width: 150px;
+        width: 162px;
         background-color: rgb(110, 138, 98);
+        box-shadow: 0 0 10px #525252;
         border-radius: 5px;
-        gap: 5px;
-        padding: 3px;
-        font-size: 1.2rem;
-        color: white;
+        font-size: 1.5rem;
+        font-variant : small-caps;
+        padding: 5px;
         
     }
 
@@ -67,16 +88,56 @@
     }
 
     .spell-panel {
-        
         display: flex;
         flex-direction: column;
-        width: 150px;
+        width: 162px;
         background-color: rgb(136, 133, 189);
+        box-shadow: 0 0 10px #525252;
         border-radius: 5px;
-        gap: 5px;
-        padding: 3px;
-        font-size: 1.2rem;
+        font-size: 1.5rem;
         color: white;
+        font-variant : small-caps;
+        padding: 5px;
+        align-items: center;
+        justify-items: space-between;
+    }
+
+    .spell-row{
+        display: flex;
+        flex-direction: row;
+        justify-content:space-between;
+    }
+
+    .spell-input {
+        width: 30px;
+        height:34px;
+        border-radius: 5px;
+        border: none;
+        background-color: #d9d9d9;
+        padding:2px;
         text-align: center;
+        font-size: 20px;
+    }
+
+    .spell-btn {
+        border-radius: 50%;
+        height: 23px;
+        width:23px;
+        font-size: 20px;
+        border: none;
+        background-color: #d9d9d9;
+    }
+
+    .notes-panel {
+        display: flex;
+        flex-direction: column;
+        width: 180px;
+        background-color: rgb(172, 127, 71);
+        box-shadow: 0 0 10px #525252;
+        border-radius: 5px;
+        font-size: 1.5rem;
+        font-variant : small-caps;
+        padding: 5px;
+        color: white;
     }
 </style>
